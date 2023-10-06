ee2a349d442cbfd98a61693cfe3dc05eca30b1dc
Felix Penzlin <eeefix@gmail.com>
2023-10-06 16:51:44 +0200

    escape double quotes

--- lib/phoenix_svg.ex
+++ lib/phoenix_svg.ex
@@ -92,7 +92,7 @@ defmodule PhoenixSVG do
     """
   end
 
-  def render("<?xml version="1.0" encoding="UTF-8"?>" <> tail, attributes, assigns) do
+  def render("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" <> tail, attributes, assigns) do
     render(String.trim(tail), attributes, assigns)
   end
 
